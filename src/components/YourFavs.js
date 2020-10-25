11c71c52350b8948b4a524ec92a3100b837fb380
bwnstk <info@flimmerkiste.xyz>
2020-10-25 17:33:45 +0100

    Add localStorage to Human Faces

--- src/components/YourFavs.js
+++ src/components/YourFavs.js
@@ -1,14 +1,45 @@
-import { removeAllChildNodes } from "../utils/helpers";
+import {
+  generateRandomSeed,
+  getBase64Image,
+  removeAllChildNodes,
+} from "../utils/helpers";
 
 const { createElement } = require("../utils/elements");
+
+let imgSrc = localStorage.getItem("imgSrc") || "";
+
+if (imgSrc === "") {
+  let randomSeed = generateRandomSeed(5);
+  imgSrc = `https://robohash.org/you-${randomSeed}.svg?set=set5`;
+  localStorage.setItem("imgSrc", imgSrc);
+  console.log("SET to localstorage");
+} else {
+  let dataSrc = localStorage.getItem("imgSrc");
+  imgSrc = dataSrc;
+  console.log("GET from localstorage");
+}
+
+const userImg = createElement("img", {
+  className: "profilePicture",
+  src: imgSrc,
+});
+// Look in localStorage nach Bildern, wenn JA Bild verwenden
+// const userImg =
+//   ? localStorage(setItem("imgData"), userImg)
+//   : console.log("wohoo");
 
 export const generateYourFavs = (favouriteCities, favCitiesBox) => {
   if (favouriteCities.length > 0) {
     let header = createElement("h6", {
       className: "robosHeader",
-      innerText: "Your Favourite Cities:",
+      innerHTML: `Your Favourite Cities:`,
     });
-    favCitiesBox.append(header) || "";
+
+    let headerBody = createElement("div", {
+      className: "headerBody",
+      children: [userImg, header],
+    });
+    favCitiesBox.append(headerBody) || "";
   } else {
     return document.createElement("div");
   }
