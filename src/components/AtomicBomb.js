80cca1774bccaea6f4d2ab50d94e47e09eca84d9
bwnstk <info@flimmerkiste.xyz>
2020-10-26 11:57:15 +0100

    Add BOOOMB

--- src/components/AtomicBomb.js
+++ src/components/AtomicBomb.js
@@ -1,10 +1,13 @@
 import "./atomicBomb.css";
 const { createElement } = require("../utils/elements");
+import bombElement from "../assets/bomb.gif";
+
+const bombImg = createElement("img", { className: "BOOOMB", src: bombElement });
 
 export function createAtomicBomb(onClick) {
   const bombButton = createElement("button", {
     className: "bombButton",
-    innerText: "💥💥🚀🚀💣💣",
+    children: [bombImg],
     ...onClick,
   });
 
